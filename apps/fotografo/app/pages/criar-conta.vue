dfc6f3cea23b7c34c1ae3d76778159c766e2491f
Alan Alves <aalanallvesgt@gmail.com>
2026-09-18 11:46:15 -0300

    fotografo: regex de acentos legivel (\p{Diacritic})

--- apps/fotografo/app/pages/criar-conta.vue
+++ apps/fotografo/app/pages/criar-conta.vue
@@ -13,7 +13,7 @@ const mostrarSenha = ref(false);
 function slugDe(texto: string) {
   return texto
     .normalize('NFD')
-    .replace(/[̀-ͯ]/g, '') // tira acentos
+    .replace(/\p{Diacritic}/gu, '') // tira acentos (NFD separa a letra do acento)
     .toLowerCase()
     .replace(/[^a-z0-9-]+/g, '-')
     .replace(/-{2,}/g, '-')
